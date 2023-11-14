37c495ee34b64e9bf71bbfc35abd6c111daa5891
Tobias Emil Harbo <tobiasharbo@gmail.com>
2023-11-14 14:47:48 +0100

    dockerized

--- src/main/java/teh/dev/companyportal/config/JerseyConfig.java
+++ src/main/java/teh/dev/companyportal/config/JerseyConfig.java
@@ -3,11 +3,13 @@ package teh.dev.companyportal.config;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.context.annotation.Configuration;
 import teh.dev.companyportal.api.CompanyApi;
+import teh.dev.companyportal.providers.PreMatchingRequestFilter;
 
 @Configuration
 public class JerseyConfig extends ResourceConfig {
 
     public JerseyConfig() {
         register(CompanyApi.class);
+        register(PreMatchingRequestFilter.class);
     }
 }
---
+++ src/main/java/teh/dev/companyportal/providers/PreMatchingRequestFilter.java
@@ -0,0 +1,16 @@
+package teh.dev.companyportal.providers;
+
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerRequestFilter;
+import jakarta.ws.rs.container.PreMatching;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@PreMatching
+public class PreMatchingRequestFilter implements ContainerRequestFilter {
+
+    @Override
+    public void filter(ContainerRequestContext ctx) {
+        log.info(ctx.getMethod() + " request on /" + ctx.getUriInfo().getPath());
+    }
+}
